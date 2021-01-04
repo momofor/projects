16796d46fd89870b5411e97c9d5d368d1c21b3c8
momofor <mohammedclacher2014@gmail.com>
2021-01-04 20:39:20 +0100

    added big patch to start-page

--- start-page/script.js
+++ start-page/script.js
@@ -26,14 +26,34 @@ create.addEventListener("click", createCard);
 function createCard() {
     var cardName = prompt("call the card");
     var cardLink = prompt("call the link");
-    //let cardClass = prompt("class for card");
-    var cardDev = document.querySelector("div.dev");
-    var card = cardDev.appendChild(document.createElement("div"));
-    card.classList.add("card", "n");
-    var finishedCard = document.querySelector(".n");
-    var anchor = finishedCard.appendChild(document.createElement("a"));
-    anchor.classList.add("link");
-    var link1 = document.querySelector(".link");
-    link1.setAttribute("href", cardLink);
-    link1.innerHTML = cardName;
+    var newType = prompt("do you want to create a new class ? yes/no");
+    if (newType === "yes") {
+        var cardType = prompt("what do you want to call the type ?");
+        var cardParentParent = document.querySelector("div.websites");
+        var addCardParent = cardParentParent.appendChild(document.createElement("div"));
+        addCardParent.classList.add(cardType, "subheader");
+        var sub = addCardParent.appendChild(document.createElement("h1"));
+        sub.classList.add("sub", cardName);
+        sub.textContent = cardType;
+        var card = addCardParent.appendChild(document.createElement("div"));
+        card.classList.add("card", cardName);
+        var finishedCard = document.querySelector(".card." + cardName);
+        var anchor = finishedCard.appendChild(document.createElement("a"));
+        anchor.classList.add(cardName);
+        var link1 = document.querySelector("a." + cardName);
+        link1.setAttribute("href", cardLink);
+        link1.innerHTML = cardName;
+    }
+    else {
+        var cardType = prompt("what type do you want the card");
+        var setCardType = document.querySelector("div." + cardType);
+        var card = setCardType.appendChild(document.createElement("div"));
+        card.classList.add("card", cardName);
+        var finishedCard = document.querySelector(".card." + cardName);
+        var anchor = finishedCard.appendChild(document.createElement("a"));
+        anchor.classList.add(cardName);
+        var link1 = document.querySelector("a." + cardName);
+        link1.setAttribute("href", cardLink);
+        link1.innerHTML = cardName;
+    }
 }
--- start-page/script.ts
+++ start-page/script.ts
@@ -1,9 +1,9 @@
 setInterval(showTime, 1000);
 function showTime() {
-	let time = new Date();
-	let hour: any = time.getHours();
-	let min: any = time.getMinutes();
-	let sec: any = time.getSeconds();
+	const time = new Date();
+	let hour: string | number = time.getHours();
+	let min: string | number = time.getMinutes();
+	let sec: string | number = time.getSeconds();
 	let am_pm = "AM";
 
 	if (hour > 12) {
@@ -19,7 +19,7 @@ function showTime() {
 	min = min < 10 ? "0" + min : min;
 	sec = sec < 10 ? "0" + sec : sec;
 
-	let currentTime = hour + ":" + min + ":" + sec + am_pm;
+	const currentTime = hour + ":" + min + ":" + sec + am_pm;
 
 	document.querySelector(".clock").innerHTML = currentTime;
 }
@@ -30,16 +30,46 @@ const create = document.querySelector("button");
 create.addEventListener("click", createCard);
 
 function createCard() {
-	let cardName = prompt("call the card");
-	let cardLink = prompt("call the link");
-	//let cardClass = prompt("class for card");
-	let cardDev = document.querySelector("div.dev");
-	let card = cardDev.appendChild(document.createElement("div"));
-	card.classList.add("card", "n");
-	let finishedCard = document.querySelector(".n");
-	let anchor = finishedCard.appendChild(document.createElement("a"));
-	anchor.classList.add("link");
-	let link1 = document.querySelector(".link");
-	link1.setAttribute("href", cardLink);
-	link1.innerHTML = cardName;
+	const cardName = prompt("call the card");
+	const cardLink = prompt("call the link");
+	const newType: string = prompt(
+		"do you want to create a new class ? yes/no"
+	);
+
+	if (newType === "yes") {
+		const cardType: string = prompt("what do you want to call the type ?");
+
+		const cardParentParent = document.querySelector("div.websites");
+
+		const addCardParent = cardParentParent.appendChild(
+			document.createElement("div")
+		);
+		addCardParent.classList.add(cardType, "subheader");
+
+		const sub = addCardParent.appendChild(document.createElement("h1"));
+		sub.classList.add("sub", cardName);
+		sub.textContent = cardType;
+
+		const card = addCardParent.appendChild(document.createElement("div"));
+		card.classList.add("card", cardName);
+		const finishedCard = document.querySelector(".card." + cardName);
+
+		const anchor = finishedCard.appendChild(document.createElement("a"));
+		anchor.classList.add(cardName);
+		const link1 = document.querySelector("a." + cardName);
+		link1.setAttribute("href", cardLink);
+		link1.innerHTML = cardName;
+	} else {
+		const cardType = prompt("what type do you want the card");
+		const setCardType = document.querySelector("div." + cardType);
+		const card = setCardType.appendChild(document.createElement("div"));
+		card.classList.add("card", cardName);
+		const finishedCard = document.querySelector(".card." + cardName);
+
+		const anchor = finishedCard.appendChild(document.createElement("a"));
+		anchor.classList.add(cardName);
+		const link1 = document.querySelector("a." + cardName);
+		link1.setAttribute("href", cardLink);
+		link1.innerHTML = cardName;
+	}
 }
