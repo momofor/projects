fbccf907e2b7d212f9db3cd77971cddf88bf8200
momofor <mohammedclacher2014@gmail.com>
2021-01-07 23:41:13 +0100

    fixed remmove todo

--- todo-app/public/script.js
+++ todo-app/public/script.js
@@ -1,11 +1,10 @@
 var task = document.querySelector("#task");
-var taskBut = document.getElementById("taskBut");
-var clear = document.querySelector("#clear");
+var taskButton = document.getElementById("taskBut");
+var clearButton = document.querySelector("#clear");
 var tasks = document.querySelector("#tasks");
 var taskCard = document.querySelector(".taskCard");
-var removeBut = document.getElementsByClassName("removeBut");
-var trashed = document.getElementsByClassName("trashed");
-taskBut.addEventListener("click", addTask);
+var removeBut = (document.getElementsByClassName("removeBut"));
+taskButton.addEventListener("click", addTask);
 function addTask() {
     var taskVal = task.value;
     var taskuContainer = tasks.appendChild(document.createElement("ul"));
@@ -17,18 +16,13 @@ function addTask() {
     taskuDelBut.classList.add("removeBut");
     taskuDelBut.innerHTML = "x";
     for (var i = 0; i < removeBut.length; i++) {
-        removeBut[i].addEventListener("click", alertTask);
-    }
-    function alertTask(e) {
-        e.parent = taskuContainer;
-        e.parent.classList.add("trashed");
-    }
-    for (var i = 0; i < trashed.length; i++) {
-        var trash = trashed[i];
-        console.log(trash);
+        removeBut[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        };
     }
 }
-clear.addEventListener("click", clearTasks);
+clearButton.addEventListener("click", clearTasks);
 function clearTasks() {
     while (tasks.firstChild) {
         tasks.removeChild(tasks.lastChild);
--- todo-app/src/script.ts
+++ todo-app/src/script.ts
@@ -1,11 +1,14 @@
 const task = <HTMLInputElement>document.querySelector("#task");
-const taskBut = <HTMLInputElement>document.getElementById("taskBut");
-const clear = <HTMLButtonElement>document.querySelector("#clear");
+const taskButton = <HTMLInputElement>document.getElementById("taskBut");
+const clearButton = <HTMLButtonElement>document.querySelector("#clear");
 const tasks = <HTMLDivElement>document.querySelector("#tasks");
 const taskCard = <HTMLLIElement>document.querySelector(".taskCard");
-const removeBut = document.getElementsByClassName("removeBut");
-let trashed = document.getElementsByClassName("trashed");
-taskBut.addEventListener("click", addTask);
+const removeBut = <HTMLCollectionOf<HTMLButtonElement>>(
+	document.getElementsByClassName("removeBut")
+);
+
+taskButton.addEventListener("click", addTask);
+
 function addTask() {
 	const taskVal = task.value;
 	const taskuContainer = tasks.appendChild(document.createElement("ul"));
@@ -19,19 +22,14 @@ function addTask() {
 	taskuDelBut.classList.add("removeBut");
 	taskuDelBut.innerHTML = "x";
 	for (let i = 0; i < removeBut.length; i++) {
-		removeBut[i].addEventListener("click", alertTask);
-	}
-	function alertTask(e: any) {
-		e.parent = taskuContainer;
-		e.parent.classList.add("trashed");
-	}
-	for (let i = 0; i < trashed.length; i++) {
-		let trash = trashed[i];
-		console.log(trash);
+		removeBut[i].onclick = function () {
+			var div = (this as HTMLButtonElement).parentElement!;
+			div.style.display = "none";
+		};
 	}
 }
 
-clear.addEventListener("click", clearTasks);
+clearButton.addEventListener("click", clearTasks);
 function clearTasks() {
 	while (tasks.firstChild) {
 		tasks.removeChild(tasks.lastChild);
