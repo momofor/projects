07ffb99f4f349626fd0522ae26733305a39b1866
momofor <mohammedclacher2014@gmail.com>
2021-01-07 17:25:42 +0100

    I HATE REACT AND REACT APP

--- react-app/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
-class Board extends React.Component {
-	renderSquare(i) {
-		return <square value={i} />;
-	}
-}
