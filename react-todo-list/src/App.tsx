679782c6bcf2423a0faf5d1c5c7b0a7d40699a73
momofor <mohammedclacher2014@gmail.com>
2021-01-14 16:41:59 +0100

    nothing special at ALL

--- react-todo-list/src/App.tsx
+++ react-todo-list/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./App.css";
 
+interface WelcomeProps {
+	user: string;
+	age: number | string;
+}
+
+const Welcome: React.FC<WelcomeProps> = (props) => {
+	return (
+		<h1>
+			Hello {props.user}, You are {props.age} old
+		</h1>
+	);
+};
+
 function App() {
-	const me = "wtf is going on in here";
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Welcome user="momofor" age="13" />
+		</div>
+	);
 }
 
 export default App;
